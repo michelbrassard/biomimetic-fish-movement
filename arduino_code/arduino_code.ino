1f6e3d0fc08668d9924f07ce827f8135da4956b3
Michel Brassard <michelbrassard47@gmail.com>
2026-03-17 21:22:17 +0100

    add initial sine functions with phase angle differences

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -12,21 +12,23 @@ Servo servo3;
 Servo servo4;
 Servo servo5;
 
+float f = 4.0;
+
 
 void setup() {
   Serial.begin(9600);
 
   servo1.attach(SERVO_PIN_1);
-  // servo2.attach(SERVO_PIN_2);
-  // servo3.attach(SERVO_PIN_3);
+  servo2.attach(SERVO_PIN_2);
+  servo3.attach(SERVO_PIN_3);
   // servo4.attach(SERVO_PIN_4);
   // servo5.attach(SERVO_PIN_5);
 
-  servo1.write(0);
-  // servo2.write(0);
-  // servo3.write(0);
-  // servo4.write(0);
-  // servo5.write(0);
+  servo1.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  // servo4.write(90);
+  // servo5.write(90);
 
   delay(2000);
 
@@ -36,11 +38,23 @@ void setup() {
 void loop() {
 
   float t = millis() / 1000.0;
-  float sineValue = 0.5 * sin((t + 0) * 3.0); // Ovdje upravljam amplitudu, vremenski offset i frekvenciju...
-  int angle = 90 + (90 * sineValue);
   
-  servo1.write(angle);
+  float sine3 = 0.1 * sin(t * f + 1.2);
+  int angle3 = (90 * sine3);
 
-  delay(15);
+  float sine2 = 0.25 * sin(t * f + 0.9);
+  int angle2 = (90 * sine2);
+
+  float sine1 = 0.35 * sin(t * f);
+  int angle1 = (90 * sine1);
+  
+  
+  servo3.write(90 + angle3); //head
+  servo2.write(90 - angle2);
+  servo1.write(90 + angle1);
+
+  delay(5);
 }
+
+
 
